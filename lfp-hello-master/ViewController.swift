be46252a9f7e01ee65bec2103230cdb357b14550
lfp <lfp@lfpdeimac.local>
2016-09-22 17:03:01 +0800

    Initial Commit

---
+++ lfp-hello-master/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lfp-hello-master
+//
+//  Created by lfp on 16/9/22.
+//  Copyright © 2016年 lfp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lfp-hello-masterTests/lfp_hello_masterTests.swift
@@ -0,0 +1,36 @@
+//
+//  lfp_hello_masterTests.swift
+//  lfp-hello-masterTests
+//
+//  Created by lfp on 16/9/22.
+//  Copyright © 2016年 lfp. All rights reserved.
+//
+
+import XCTest
+@testable import lfp_hello_master
+
+class lfp_hello_masterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
